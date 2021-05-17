a72eae878e80c33bb2a42e67f7bf44add240b930
abhijeetmali018 <83824260+abhijeetmali018@users.noreply.github.com>
2021-05-17 09:05:22 +0530

    Add files via upload

---
+++ Q12.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Q12
+{
+	public static void main(String args[])
+	{	
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter 3 nos");
+	
+		int i = sc.nextInt();
+		int j = sc.nextInt();
+		int k = sc.nextInt();
+		
+		int average = (i+j+k)/3;
+		
+		System.out.println("Averge of 3 nos = "+average );
+		
+		
+	}
+	
+}
