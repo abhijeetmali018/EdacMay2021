38fe4ce0c59a2ca45b076189f6c9b334a773081d
abhijeetmali018 <83824260+abhijeetmali018@users.noreply.github.com>
2021-05-21 13:22:53 +0530

    Add files via upload

---
+++ BPC/Assignment 1/Q3.java
@@ -0,0 +1,8 @@
+class Q3
+{
+	public static void main(String args[])
+	{
+		int i=50/3;
+		System.out.print(" Quotient for 50/3 = "+i);
+	}	
+}	
---
+++ BPC/Assignment 1/Q5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Q5
+{
+	public static void main(String args[])
+	{
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.println(" Input First Number");
+	int i = sc.nextInt();
+	
+	
+	System.out.println(" Input Second Number");
+	int j = sc.nextInt();
+	
+	int k = i*j;
+	System.out.println(i*j+"=" +k);
+	}
+	
+}
+
+
